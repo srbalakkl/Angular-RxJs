34d7369999ab9a38d814549b9334487c4b394844
bala <sr.bala.kkl@gmail.com>
2023-12-28 08:16:20 +0530

    improve:replay subject

--- src/app/replay-subject/replay-subject.component.ts
+++ src/app/replay-subject/replay-subject.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
-import {Observable, ReplaySubject, Subject} from "rxjs";
-import {writeErrorToLogFile} from "@angular/cli/src/utilities/log-file";
+import {BehaviorSubject, Observable, ReplaySubject, Subject} from "rxjs";
 
 @Component({
   selector: 'app-replay-subject',
@@ -11,36 +10,39 @@ import {writeErrorToLogFile} from "@angular/cli/src/utilities/log-file";
 })
 export class ReplaySubjectComponent {
   constructor() {
-    //
-    // /**
-    //  * @description Here <b> new Subject() </b> instance would print only <b> 2 and 3 </b>
-    //  * */
-    // const replaySubject = new Subject();
-    // // const replaySubject = new ReplaySubject();
-    // replaySubject.next(1)
-    //
-    // replaySubject.subscribe((value) => {
-    //   console.log('\nobserver 1', value);
-    // });
-    //
-    // replaySubject.next(2)
-    // replaySubject.next(3)
-    //
-    // replaySubject.subscribe((value) => {
-    //   console.log('\nobserver 2', value);
-    // })
+    /**
+     * @description Here <b> new Subject() </b> instance would print only <b> 2 and 3 </b>
+     * */
 
+    // const replaySubject = new Subject();
+    // const replaySubject = new BehaviorSubject(0);
+    const replaySubject = new ReplaySubject();
+    replaySubject.next(1)
+
+    replaySubject.subscribe((value) => {
+      console.log('\nobserver 1', value);
+    });
+
+    replaySubject.next(2)
+    replaySubject.next(3)
+
+    replaySubject.subscribe((value) => {
+      console.log('\nobserver 2', value);
+    })
+
+    replaySubject.next(4)
     // this.replaySubjectWithError();
-    this.withBufferSize()
+    // this.withBufferSize()
+    // this.withBufferTime()
 
   }
 
 
-  replaySubjectWithError(){
+  replaySubjectWithError() {
     /**
      * @description ReplaySubject can emmit value even after the error is accrued.
      * */
-    // const replaySubject = new Subject();
+      // const replaySubject = new Subject();
     const replaySubject = new ReplaySubject();
     replaySubject.next(1)
 
@@ -59,7 +61,7 @@ export class ReplaySubjectComponent {
     })
   }
 
-  withBufferSize(){
+  withBufferSize() {
     /**
      * @description
      * BufferSize - Define How many items are stored in an array,Default is infinite,
@@ -83,6 +85,29 @@ export class ReplaySubjectComponent {
     replaySubject.subscribe((value) => {//<- here it show only one value,becz of size limit
       console.log('\nobserver 3', value);
     })
+  }
+
+  withBufferTime() {
+    const replaySubject = new ReplaySubject(Infinity, 1000);
+    replaySubject.next(1)
+
+    replaySubject.subscribe((value) => {
+      console.log('\nobserver 1', value);
+    });
+
+    replaySubject.next(2)
+    replaySubject.next(3)
+
+    replaySubject.subscribe((value) => {//<- here it show only one value,becz of size limit
+      console.log('\nobserver 2', value);
+    })
+
+    setTimeout(() => {
+      replaySubject.subscribe((value) => {//<- here it show only one value,becz of size limit
+        console.log('\nobserver 3', value);
+      })
+    },2000);
+
   }
 
 //   todo:buffer time here
