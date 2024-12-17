2944ab8f72078eb6f5add67372c65899f3a17eaa
bala <sr.bala.kkl@gmail.com>
2024-12-17 22:55:30 +0530

    doc: distinct-until-changed-operator.component.html

--- src/app/prevent-memory-leaks/prevent-memory-leaks.component.ts
+++ src/app/prevent-memory-leaks/prevent-memory-leaks.component.ts
@@ -3,10 +3,11 @@ import {interval, Subject, takeUntil} from "rxjs";
 import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
-    selector: 'app-prevent-memory-leaks',
-    imports: [],
-    templateUrl: './prevent-memory-leaks.component.html',
-    styleUrl: './prevent-memory-leaks.component.sass'
+  selector: 'app-prevent-memory-leaks',
+  imports: [],
+  templateUrl: './prevent-memory-leaks.component.html',
+  standalone: true,
+  styleUrl: './prevent-memory-leaks.component.sass'
 })
 export class PreventMemoryLeaksComponent implements OnInit, OnDestroy {
   private unsubscribe$: Subject<void> = new Subject<void>();
@@ -14,7 +15,11 @@ export class PreventMemoryLeaksComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     interval(1000).pipe(
       takeUntil(this.unsubscribe$),
-      // takeUntilDestroyed(this.destroyRef)//another way to prevent m/y leaks. but, it works only after @angular/core16 version.
+      takeUntilDestroyed(this.destroyRef)//another way to prevent m/y leaks,but it works only after @angular/core16 version.
+      // note:
+      // takeUntilDestroyed()
+      // operator must be put at the end of the line because it will now work for downstream observable
+      // and cause m/y leak if placed in the wrong place
       ).subscribe(r=>console.log(r));
   }
 
