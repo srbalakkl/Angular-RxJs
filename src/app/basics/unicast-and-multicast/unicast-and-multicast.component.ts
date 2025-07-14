b484435e8cca6d3cc605ec570fc17499a47b8160
bala <sr.bala.kkl@gmail.com>
2025-07-14 18:07:16 +0530

    DOC: unicast and multicast doc

--- src/app/basics/unicast-and-multicast/unicast-and-multicast.component.ts
+++ src/app/basics/unicast-and-multicast/unicast-and-multicast.component.ts
@@ -13,16 +13,18 @@ export class UnicastAndMulticastComponent implements OnInit {
     let observable = new Observable((subscriber) => {
       subscriber.next(Math.random());
       subscriber.complete();
-    })
+    })//<- observable is unicast.
 
     observable.subscribe(r => console.log('observable 1 = ', r));
     observable.subscribe(r => console.log('observable 2 = ', r));
 
+    let subject = new Subject();//<- subject is multicast
 
-    let subject = new Subject();
-
-    // subject.next(Math.random());   //<- since the `subject` is the hot observable, data will be returned immediately.
+    // subject.next(Math.random());   //<- since the `subject` is the HOT OBSERVABLE, data will be returned immediately.
     // so the value will not be emitted here.
+
+    // or to say in another way, HOT OBSERVABLE is like watching a movie in a theater: The movie won't wait for you to start.
+    // Just like `subject.next(Math.random())` line
 
     subject.subscribe(r => console.log('subject 1 =', r));
     subject.subscribe(r => console.log('subject 2 =', r));
--- src/app/signals/effect/effect.component.ts
+++ src/app/signals/effect/effect.component.ts
@@ -18,13 +18,13 @@ export class EffectComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('Normal Greet =', this.greet())
+    console.log('Normal Greet 1 =', this.greet())
 
     setTimeout(() => {
       this.greet.set('hello..')
     }, 1000)
 
-    console.log('Normal Greet =', this.greet())
+    console.log('Normal Greet 2 =', this.greet())
   }
 
 }
